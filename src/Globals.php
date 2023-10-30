cfb53f2818cab6759c98f251bca6b5e348405680
nixs-dev <joabewick@gmail.com>
2023-10-30 18:49:23 -0300

    DEFAULT_STORAGE_PATH Global added

--- src/Globals.php
+++ src/Globals.php
@@ -6,6 +6,7 @@ namespace PHPUtils;
 class Globals {
     public static $ROOT;
     public static $ROUTER;
+    public static $DEFAULT_STORAGE_PATH;
     
     public static function set($attribute, $value) {
         self::${$attribute} = $value;
